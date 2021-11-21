91780e2de4fd05094fe4abc12a3997503da95de4
naokiuma <sagalumaata@gmail.com>
2021-11-21 13:27:33 +0900

    recordコンポーネント作成

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001
+const mysql = require('mysql');
 //localhost:3000でアクセス
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password: 'root',
+    database: 'ex_timer',
+    port: 8889
+})
+let _result;
+connection.connect;
+connection.query('select * from user',
+    (err,result) => {
+        if(err) {
+            throw err;
+        }
+        console.log("success");
+        _result = result;
+    }
+)
+connection.end();
 
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
@@ -18,7 +39,9 @@ app.get('/',(req,res) =>{
 
 app.get('/api',(req,res) => {//api専用のurl
     
-    res.json({message:"hello world"});
+    // res.json({message:"hello world"});
+    res.json({message:_result});
+
 })
 
 app.listen(port,()=>{
